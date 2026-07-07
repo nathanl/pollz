064bdad179aae1814403e4090c7b667be5a64b99
Nathan Long <hello@nathanmlong.com>
2026-07-07 09:23:06 -0400

    Update deps

--- mix.exs
+++ mix.exs
@@ -47,7 +47,7 @@ defmodule Pollz.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 1.1.0"},
+      {:phoenix_live_view, "~> 1.2"},
       {:lazy_html, ">= 0.1.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.10", runtime: Mix.env() == :dev},
